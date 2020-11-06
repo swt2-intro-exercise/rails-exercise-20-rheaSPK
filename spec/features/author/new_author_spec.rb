7e007d26fcd6bd019b2ac67ac7bfaef9db649e93
Simon-Pierre Krautkraemer <simon-pierre.krautkraemer@student.hpi.de>
2020-11-06 17:14:57 +0100

    fix #2

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,11 @@ describe "New authors page", type: :feature do
   it "should render withour error" do
     visit new_author_path
   end
+
+  it "should have text inputs for an author's first name, last name, and homepage" do
+    visit new_author_path
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
+  end
 end
