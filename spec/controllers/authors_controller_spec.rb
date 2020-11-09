7f4ca36d460690859d4e7207a1b807754ae0ead3
Simon-Pierre Krautkraemer <simon-pierre.krautkraemer@student.hpi.de>
2020-11-09 12:14:33 +0100

    fixed #3

---
+++ spec/controllers/authors_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe AuthorsController, type: :model do
+  it 'an author has a first name, a last name and a homepage' do
+    author = Author.new(first_name: "Max", last_name: "Mustermann", homepage: "google.de")
+    expect(author.first_name).to eq "Max"
+    expect(author.last_name).to eq "Mustermann"
+    expect(author.homepage).to eq "google.de"
+  end
+end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 describe "New authors page", type: :feature do
 
   it "should render withour error" do
-    visit new_author_path
+    visit new_authors_path
   end
 
   it "should have text inputs for an author's first name, last name, and homepage" do
-    visit new_author_path
+    visit new_authors_path
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
 end
