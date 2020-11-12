528dba8d67d584408f375345a59d9a6e12d32f69
Simon-Pierre Krautkraemer <simon-pierre.krautkraemer@student.hpi.de>
2020-11-12 18:29:58 +0100

    fix #7

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,21 @@
 class AuthorsController < ApplicationController
   def new
+
   end
 
   def show
 
   end
+
+  def create
+    @author = Author.new(author_params)
+
+    @author.save
+    redirect_to root_path
+  end
+
+  private
+    def author_params
+      params.require(:author).permit(:first_name, :last_name, :homepage)
+    end
 end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -12,4 +12,13 @@ describe "New authors page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should create a new author" do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Philipp'
+    page.fill_in 'author[last_name]', with: 'Naumann'
+    page.fill_in 'author[homepage]', with: 'HPI.de'
+    find('input[type="submit"]').click
+    expect(Author.count).to eq 1
+  end
 end
