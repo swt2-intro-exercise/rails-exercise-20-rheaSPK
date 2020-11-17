cb1956b26c46a6ce2641b464d2786bd944df1dea
Simon-Pierre Krautkraemer <simon-pierre.krautkraemer@student.hpi.de>
2020-11-17 16:36:16 +0100

    fix #14 link to edit page

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -5,11 +5,12 @@ describe 'The index page of all authors' do
     @alan = FactoryBot.create(:author)
     visit authors_path
   end
-  it 'renders html table with name homepage and detailed page link' do
+  it 'renders html table with name homepage, detailed page link and edit link' do
     assert_selector('table')
     assert_selector('th', text:'Name')
     assert_selector('th', text:'Homepage')
     assert_selector('th', text:'Link')
+    assert_selector('th', text:'Edit')
   end
 
   it 'renders a link for a new author' do
@@ -23,5 +24,9 @@ describe 'The index page of all authors' do
 
   it 'links to the correct detail page' do
     expect(page).to have_link 'Details', href: author_path(@alan)
+  end
+
+  it 'links to the correct edit page' do
+    expect(page).to have_link 'Edit', href: edit_author_path(@alan)
   end
 end
