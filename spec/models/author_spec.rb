32c99d66ccd66ae3d83e97d0e681f932b6c3c019
Simon-Pierre Krautkraemer <simon-pierre.krautkraemer@student.hpi.de>
2020-11-17 10:09:10 +0100

    fix #10 validates last name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   context 'an author was created'  do
     author = Author.new(first_name: "Max", last_name: "Mustermann", homepage: "google.de")
+    invalid_author = Author.new(first_name:"Felix",last_name:nil, homepage: "to.cool.for.a.last.name.org")
+
 
     it 'has a first name, a last name and a homepage' do
       expect(author.first_name).to eq "Max"
@@ -12,6 +14,10 @@ RSpec.describe Author, type: :model do
 
     it 'has a full name' do
       expect(author.name).to eq "Max Mustermann"
+    end
+
+    it 'has a last name' do
+      expect(invalid_author).to_not be_valid
     end
   end
 end
