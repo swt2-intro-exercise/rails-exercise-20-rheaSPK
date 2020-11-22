19e15ba3ddb0dc390270cf52d8bf95587bfa18bb
Simon-Pierre <simon-pierre.krautkraemer@student.hpi.de>
2020-11-22 10:44:26 +0100

    fix #18 validates venue

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  invalid_paper = Paper.new(title: nil, venue: 'HPI', year: 2020)
+  paper_without_title = Paper.new(title: nil, venue: 'HPI', year: 2020)
+  paper_without_venue = Paper.new(title: 'Nice to meet you', venue: nil, year: 2020)
 
   it 'has a Title' do
-    expect(invalid_paper).to_not be_valid
+    expect(paper_without_title).to_not be_valid
+  end
+
+  it 'has a venue' do
+    expect(paper_without_venue).to_not be_valid
   end
 end
