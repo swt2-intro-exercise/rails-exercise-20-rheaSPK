ce6257ecfbf2f37f92e55c37a7f76bf8afc2eb07
Simon-Pierre <simon-pierre.krautkraemer@student.hpi.de>
2020-11-22 10:47:46 +0100

    fix #19 validates year

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Paper, type: :model do
   paper_without_title = Paper.new(title: nil, venue: 'HPI', year: 2020)
   paper_without_venue = Paper.new(title: 'Nice to meet you', venue: nil, year: 2020)
+  paper_without_year = Paper.new(title: 'Nice to meet you', venue: 'HPI', year: nil)
 
   it 'has a Title' do
     expect(paper_without_title).to_not be_valid
@@ -10,5 +11,9 @@ RSpec.describe Paper, type: :model do
 
   it 'has a venue' do
     expect(paper_without_venue).to_not be_valid
+  end
+
+  it 'has a year' do
+    expect(paper_without_year).to_not be_valid
   end
 end
