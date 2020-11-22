1f1b1ff50d4450b3abe5bda636b0f855c7208e42
Simon-Pierre <simon-pierre.krautkraemer@student.hpi.de>
2020-11-22 11:16:24 +0100

    fix #21 add one to many relationship

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Paper, type: :model do
   paper_without_venue = Paper.new(title: 'Nice to meet you', venue: nil, year: 2020)
   paper_without_year = Paper.new(title: 'Nice to meet you', venue: 'HPI', year: nil)
   paper_with_non_Integer_Year = Paper.new(title: 'Nice to meet you', venue: 'HPI', year: 'twothousandtwenty')
+  valid_paper = Paper.new(title: 'Nice to meet you', venue: 'HPI', year: 2020)
 
   it 'has a Title' do
     expect(paper_without_title).to_not be_valid
@@ -20,5 +21,9 @@ RSpec.describe Paper, type: :model do
 
   it 'has a non-integer year' do
     expect(paper_with_non_Integer_Year).to_not be_valid
+  end
+
+  it 'has an (empty) list of authors' do
+    expect(valid_paper.authors).to_not be_nil
   end
 end
