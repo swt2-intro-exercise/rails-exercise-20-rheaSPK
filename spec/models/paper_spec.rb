7418e3bcf27d6805903da66ba7510d6a25acfc36
Simon-Pierre <simon-pierre.krautkraemer@student.hpi.de>
2020-11-22 10:38:45 +0100

    fix #17 validates presence of title

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  invalid_paper = Paper.new(title: nil, venue: 'HPI', year: 2020)
+
+  it 'has a Title' do
+    expect(invalid_paper).to_not be_valid
+  end
 end
