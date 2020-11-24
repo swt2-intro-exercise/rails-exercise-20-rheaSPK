c681835b2f3852f330004cf22c5b3a21fa78fd64
Simon-Pierre <simon-pierre.krautkraemer@student.hpi.de>
2020-11-24 14:47:51 +0100

    fix #25 save changes to author list

--- spec/features/paper/edit_papers_spec.rb
+++ spec/features/paper/edit_papers_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 describe 'New papers page', type: :feature do
   before 'render page' do
-    @author = Author.new(first_name: 'Gilbert', last_name: 'von Hotzenplotz', homepage: 'sexy_grannies.to')
-    @paper = Paper.create!(title: 'Sex in old age', venue: 'erotic shop around the corner', year: 2020, authors: [@author])
-    visit edit_paper_path(@author)
+    @gilbert = Author.create!(first_name: 'Gilbert', last_name: 'von Hotzenplotz', homepage: 'sexy_grannies.to')
+    @otto = Author.create!(first_name: 'Otto', last_name: 'von Bismark', homepage: 'kaiser.reich')
+    @paper = Paper.create!(title: 'Sex in old age', venue: 'erotic shop around the corner', year: 2020, authors: [])
+    visit edit_paper_path(@paper)
   end
 
   it 'has an author selector' do
     assert_selector('select[multiple]')
+  end
+
+  it 'saves authors for that page' do
+    select = page.find('select[multiple]')
+    select @gilbert.name
+    select @otto.name
+    find('input[type="submit"]').click
+    @paper.reload
+    expect(@paper.authors).to include @otto
+    expect(@paper.authors).to include @gilbert
   end
 end
